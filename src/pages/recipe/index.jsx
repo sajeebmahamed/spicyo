857738286676b4bf2b5578703e41c743105d61b4
Sajeeb <sajeebahamed6@gmail.com>
2021-02-21 18:19:25 +0600

    blog page implemented

--- src/pages/recipe/index.jsx
+++ src/pages/recipe/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainFooter from '../../components/footer';
 import Navbar from '../../components/nav';
+import TopBanner from '../../components/topBanner';
 
 function Recipe() {
     return (
         <div className="wrapper">
             <div id="content">
                 <Navbar />
+                <TopBanner value="Recipe" />
                 <MainFooter />
             </div>
         </div>
